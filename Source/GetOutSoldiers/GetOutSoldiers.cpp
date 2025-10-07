0690d936d36db3ae66b2b82402a18057f2d832fb
Haiito92 <hannaantoinebusiness@gmail.com>
2025-10-07 14:37:31 +0200

    [GP] Switch to cpp

---
+++ Source/GetOutSoldiers/GetOutSoldiers.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GetOutSoldiers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GetOutSoldiers, "GetOutSoldiers" );
