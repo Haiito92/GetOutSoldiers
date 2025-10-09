77b7a26c61d975f2017929b50a642b5743c51de3
Haiito92 <hannaantoinebusiness@gmail.com>
2025-10-09 10:15:11 +0200

    [GP] TriggerViewVolume

---
+++ Source/GetOutSoldiers/Public/Camera/Volumes/TriggerViewVolume.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ViewVolume.h"
+#include "TriggerViewVolume.generated.h"
+
+class UBoxComponent;
+
+UCLASS(Blueprintable)
+class GETOUTSOLDIERS_API ATriggerViewVolume : public AViewVolume
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATriggerViewVolume();
+
+private:
+	UFUNCTION()
+	void OnColliderBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+								const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnColliderEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UBoxComponent> m_Collider;
+	UPROPERTY(EditAnywhere, DisplayName="Target")
+	TObjectPtr<AActor> m_Target;
+};
